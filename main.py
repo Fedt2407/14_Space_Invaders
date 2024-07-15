8c0a0d1a2fb937af7250c8b0ca9c97146832d7e3
Fedt2407 <federico.tognetti@gmail.com>
2024-07-15 10:31:53 +0200

    Improved alien shape

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import pygame
 import random
+from aliens import Alien
 
-# Pygame initialization
+# Initialization of Pygame
 pygame.init()
 
-# Game window dimensions
+# Window dimensions
 window_width = 800
 window_height = 600
 
-# Creating the game window
+# Create the game window
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Space Invaders")
 
 # Colors
 black = (0, 0, 0)
 white = (255, 255, 255)
-green = (0, 255, 0)
 
 # Variables for the spaceship
 spaceship_width = 64
@@ -24,25 +24,17 @@ spaceship_x = window_width // 2 - spaceship_width // 2
 spaceship_y = window_height - spaceship_height - 10
 spaceship_speed = 5
 
-# Variables for the aliens
-alien_width = 50
-alien_height = 50
-alien_speed = 2
+# Create initial aliens
 aliens = []
-
-# Create aliens
-for i in range(5):
-    x = random.randint(0, window_width - alien_width)
+for i in range(1):
+    x = random.randint(0, window_width - 50)
     y = random.randint(50, 150)
-    aliens.append(pygame.Rect(x, y, alien_width, alien_height))
+    aliens.append(Alien(x, y))
 
 def draw_spaceship(x, y):
     pygame.draw.rect(window, white, (x, y, spaceship_width, spaceship_height))
 
-def draw_alien(alien):
-    pygame.draw.rect(window, green, alien)
-
-# Game cycle
+# Game loop
 running = True
 while running:
     window.fill(black)
@@ -51,30 +43,22 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Movement of the spaceship
+    # Spaceship movement
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and spaceship_x > 0:
         spaceship_x -= spaceship_speed
     if keys[pygame.K_RIGHT] and spaceship_x < window_width - spaceship_width:
         spaceship_x += spaceship_speed
 
-    # Movement of the aliens
+    # Update and draw aliens
     for alien in aliens:
-        alien.x += alien_speed
-        if alien.x <= 0 or alien.x >= window_width - alien_width:
-            alien_speed *= -1
-            for a in aliens:
-                a.y += alien_height // 2
+        alien.update(window_width)
+        alien.draw(window)
 
     # Draw the spaceship
     draw_spaceship(spaceship_x, spaceship_y)
-
-    # Disegna gli alieni
-    for alien in aliens:
-        draw_alien(alien)
 
     pygame.display.update()
 
 # Close the game
 pygame.quit()
-
---
+++ spaceship.py
@@ -0,0 +1,40 @@
+import pygame
+
+class Spaceship:
+    def __init__(self, x, y, cell_size=11):
+        self.x = x
+        self.y = y
+        self.cell_size = cell_size
+        self.color = (0, 255, 0)
+        
+        # Grid-based shape of the alien
+        self.shape = [
+            [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+            [0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0],
+            [0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0],
+            [0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0],
+            [0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0],
+            [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+            [0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0],
+            [1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1],
+            [1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1]
+        ]
+
+        self.width = len(self.shape[0]) * self.cell_size
+        self.height = len(self.shape) * self.cell_size
+        self.rect = pygame.Rect(x, y, self.width, self.height)
+        self.speed = 2
+
+    def draw(self, window):
+        for i, row in enumerate(self.shape):
+            for j, cell in enumerate(row):
+                if cell:
+                    pygame.draw.rect(window, self.color, (self.x + j * self.cell_size, self.y + i * self.cell_size, self.cell_size, self.cell_size))
+
+    def update(self, window_width):
+        self.x += self.speed
+        if self.x <= 0 or self.x >= window_width - self.width:
+            self.speed *= -1
+            self.y += self.cell_size * 2
+        self.rect.topleft = (self.x, self.y)
