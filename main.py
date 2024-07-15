367b5accb2d65f9c92da5633bd65a2d34be5b600
Fedt2407 <federico.tognetti@gmail.com>
2024-07-15 10:52:09 +0200

    Implemented spaceship

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
 import random
 from aliens import Alien
+from spaceship import Spaceship
 
 # Initialization of Pygame
 pygame.init()
 
 # Window dimensions
-window_width = 800
-window_height = 600
+window_width = 1800
+window_height = 1000
 
 # Create the game window
 window = pygame.display.set_mode((window_width, window_height))
@@ -18,11 +19,7 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 
 # Variables for the spaceship
-spaceship_width = 64
-spaceship_height = 64
-spaceship_x = window_width // 2 - spaceship_width // 2
-spaceship_y = window_height - spaceship_height - 10
-spaceship_speed = 5
+spaceship = Spaceship(window_width // 2 - 30, window_height - 100)
 
 # Create initial aliens
 aliens = []
@@ -32,7 +29,7 @@ for i in range(1):
     aliens.append(Alien(x, y))
 
 def draw_spaceship(x, y):
-    pygame.draw.rect(window, white, (x, y, spaceship_width, spaceship_height))
+    pygame.draw.rect(window, white, (x, y, spaceship.width, spaceship.height))
 
 # Game loop
 running = True
@@ -45,10 +42,10 @@ while running:
 
     # Spaceship movement
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and spaceship_x > 0:
-        spaceship_x -= spaceship_speed
-    if keys[pygame.K_RIGHT] and spaceship_x < window_width - spaceship_width:
-        spaceship_x += spaceship_speed
+    if keys[pygame.K_LEFT] and spaceship.x > 0:
+        spaceship.x -= spaceship.speed
+    if keys[pygame.K_RIGHT] and spaceship.x < window_width - spaceship.width:
+        spaceship.x += spaceship.speed
 
     # Update and draw aliens
     for alien in aliens:
@@ -56,7 +53,7 @@ while running:
         alien.draw(window)
 
     # Draw the spaceship
-    draw_spaceship(spaceship_x, spaceship_y)
+    spaceship.draw(window)
 
     pygame.display.update()
 
